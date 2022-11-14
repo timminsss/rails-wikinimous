f23629486eac10b650dd33e6b254614137d33e41
Shelley Timmins <shelley.timmins@hotmail.co.uk>
2022-11-14 16:42:46 +1100

    wiki copy app created

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # resources :articles, only: [:index]
+  get '/articles', to: 'articles#index'
+
+  get '/articles/new', to: 'articles#new', as: 'create_article'
+  post 'articles', to: 'articles#create'
+
+  get '/articles/:id/edit', to: 'articles#edit', as: 'edit_article'
+  patch 'articles/:id', to: 'articles#update'
+
+  get '/articles/:id', to: 'articles#show', as: :article
+
+  delete 'articles/:id', to: 'articles#destroy'
+
 end
